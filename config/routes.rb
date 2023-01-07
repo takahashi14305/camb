05d93d0478782c78fc27b0501fe0ad089e51ac70
takahashi14305 <117267359+takahashi14305@users.noreply.github.com>
2023-01-07 04:25:55 +0000

    いいね機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
   devise_for :users
-  get 'homes/top'
+  root to: 'homes#top'
+  get "about" => "homes#about"
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
