05a284d3355bf725f3fe58ce7e639c64cb7c4952
takahashi14305 <117267359+takahashi14305@users.noreply.github.com>
2023-01-08 14:57:23 +0000

    update

--- db/schema.rb
+++ db/schema.rb
@@ -41,8 +41,8 @@ ActiveRecord::Schema.define(version: 2023_01_07_041651) do
   end
 
   create_table "favorites", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "post_image_id"
+    t.integer "user_id", null: false
+    t.integer "post_image_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -69,7 +69,7 @@ ActiveRecord::Schema.define(version: 2023_01_07_041651) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
