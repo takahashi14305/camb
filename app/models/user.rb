3d8e456fc2fd89cf9daf5f2e0dec28dd9d4f7970
takahashi14305 <117267359+takahashi14305@users.noreply.github.com>
2023-01-19 13:47:52 +0000

    レイアウト調整中

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,7 +36,7 @@ class User < ApplicationRecord
   def self.guest
     find_or_create_by!(email: 'guest@exp.com') do |user|
       user.password = SecureRandom.urlsafe_base64
-      user.name = "ゲストユーザー"
+      user.name = "ゲスト"
       # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
       # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
     end
