8b0f8d952824c9693713e06817cc71243b8c0ba6
takahashi14305 <117267359+takahashi14305@users.noreply.github.com>
2023-01-25 06:56:47 +0000

    調整

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -14,11 +14,49 @@ class Public::RelationshipsController < ApplicationController
   def followings
     @user = User.find(params[:user_id])
     @users = @user.followings.page(params[:page]).per(10)
+    @currentRoomUser = RoomUser.where(user_id: @current_user)  #current_userが既にルームに参加しているか判断
+    @receiveUser = RoomUser.where(user_id: @user.id)  #他の@userがルームに参加しているか判断
+
+    unless @user.id == @current_user  #current_userと@userが一致していなければ
+      @currentRoomUser.each do |cu|    #current_userが参加していルームを取り出す
+        @receiveUser.each do |u|    #@userが参加しているルームを取り出す
+          if cu.room_id == u.room_id    #current_userと@userのルームが同じか判断(既にルームが作られているか)
+            @have_room = true    #falseの時(同じじゃない時)の条件を記述するために変数に代入
+            @room = cu.room   #ルームが共通しているcurrent_userと@userに対して変数を指定
+          end
+        end
+      end
+      unless @have_room    #ルームが同じじゃなければ
+        #新しいインスタンスを生成
+        @room = Room.new
+        @RoomUser = RoomUser.new
+        #//新しいインスタンスを生成
+      end
+    end
   end
 
   def followers
     @user = User.find(params[:user_id])
     @users = @user.followers.page(params[:page]).per(10)
+    @currentRoomUser = RoomUser.where(user_id: @current_user)  #current_userが既にルームに参加しているか判断
+    @receiveUser = RoomUser.where(user_id: @user.id)  #他の@userがルームに参加しているか判断
+
+    unless @user.id == @current_user  #current_userと@userが一致していなければ
+      @currentRoomUser.each do |cu|    #current_userが参加していルームを取り出す
+        @receiveUser.each do |u|    #@userが参加しているルームを取り出す
+          if cu.room_id == u.room_id    #current_userと@userのルームが同じか判断(既にルームが作られているか)
+            @have_room = true    #falseの時(同じじゃない時)の条件を記述するために変数に代入
+            @room = cu.room   #ルームが共通しているcurrent_userと@userに対して変数を指定
+          end
+        end
+      end
+      unless @have_room    #ルームが同じじゃなければ
+        #新しいインスタンスを生成
+        @room = Room.new
+        @RoomUser = RoomUser.new
+        #//新しいインスタンスを生成
+      end
+    end
   end
 
   def ensure_normal_user
